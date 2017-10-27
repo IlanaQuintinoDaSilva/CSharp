ab4e03c0c78489fdc4ce55857b758006c4c1cfdf
IlanaQuintinoDaSilva <ilana.quintino@gmail.com>
2017-10-27 12:46:40 -0200

    testando - código inicial

---
+++ Treinando/Treinando/InequaltyTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinando
+{
+    class InequaltyTest
+    {
+        public void Testing()
+        {
+            Console.WriteLine("Oi");
+            Console.ReadLine();
+        }
+    }
+}
